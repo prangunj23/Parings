131bc944ae96a3be40dec8be0a9314e7e22b2667
unknown <prangunj23@ncs.charter.k12.de.us>
2023-01-22 16:59:33 -0500

    code

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+
+  const 
+
+  return (
+    <div className="App">
+      <div className="form">
+        <label>Firstname:</label>
+        <input type="text" name="firstname" />
+        <label>Lastname:</label>
+        <input type="text" name="lastname" />
+        <label>Rating</label>
+        <input type="number" name="rating" />
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
